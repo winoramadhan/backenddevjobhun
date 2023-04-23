ada11f5374761476ef1cb882a77020aff0170bc1
Wino Rahmat Ramadhan <winoramadhan@gmail.com>
2023-04-23 16:34:42 +0700

    Update main.go

--- main.go
+++ main.go
@@ -7,3 +7,21 @@ import (
     "github.com/jmoiron/sqlx"
     
 )
+
+type mahasiswa struct{
+    ID int
+    Nama string
+    Usia int
+    Gender int
+    Registrasi int
+}
+
+type jurusan struct{
+    ID int
+    Jurusan string
+}
+
+type hobi struct{
+    ID mahasiswa
+    ID hobi
+}
