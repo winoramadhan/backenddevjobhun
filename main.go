44a6a2a436059977a498d737f8069b39c3a35a1c
Wino Rahmat Ramadhan <winoramadhan@gmail.com>
2023-04-23 20:44:28 +0700

    Update main.go

--- main.go
+++ main.go
@@ -8,19 +8,42 @@ import (
 )
 
 type mahasiswa struct{
-    ID int
-    Nama string
-    Usia int
-    Gender int
-    Registrasi int
+    ID          int     `json:"id"`
+    Nama        string  `json:"nama"`
+    Usia        int     `json:"usia"`
+    Gender      int     'json:"gender"`
+    Registrasi  int     'json:"registrasi"`
 }
 
 type jurusan struct{
-    ID int
-    Jurusan string
+    ID      int         `json:"id"`
+    Jurusan string      `json:"jurusan"`
 }
 
 type hobi struct{
     ID mahasiswa
     ID hobi
+}
+
+
+func (c *Client) CreatePost(content string) (id int64, err error) {
+	// run the insert script to db
+	result, err := c.DB.Exec(`
+		INSERT INTO post (content, create_time)
+		VALUES (?, now())
+	`,
+		content)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// get the last insert id
+	id, err = result.LastInsertId()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	return
 }
